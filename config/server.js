e8a5c45077caf862f8339e45820395e81602cf07
José Genilton <genilton.jr@gmail.com>
2021-09-21 22:08:08 -0300

    Primeiro Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5fd21703d1dc8818e9253b7394f73ec7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '67454968-f5b8-4f77-97b1-c6190ea2df62'
+};
